d3c1583a47b0c1d6d6311c73c78cdf96d958c9dd
Saubhagya530 <168637390+Saubhagya530@users.noreply.github.com>
2024-06-22 12:25:35 +0530

    Add files via upload
    Sorted array using Bubble sort

---
+++ bubblesort.py
@@ -0,0 +1,15 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def bubble(list_b):
+...     index_length = lenth(list_b)-1
+...     sorted = False
+...     while not sorted:
+...         sorted = True
+...         for i in range(0,index_length):
+...             if list_b[i]>list_b[i+1]:
+...                 sorted = False
+...                 list_b[i],list_b[i+1]=list[i+1],list_b[i]
+...                 return list_b
+...             print(bubble[10,20,11,45,23,90,43,99])
+>>> [DEBUG ON]
+>>> [DEBUG OFF]
